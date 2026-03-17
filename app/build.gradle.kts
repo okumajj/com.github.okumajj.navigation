c060f3078a690bc680b671bc33136db904e32036
okumajj <169715669+okumajj@users.noreply.github.com>
2026-03-17 16:17:30 -0300

    Adiciona dependência androidx.navigation:navigation-compose para suporte à navegação entre telas com Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -47,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation("androidx.navigation:navigation-compose:2.6.0")
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
